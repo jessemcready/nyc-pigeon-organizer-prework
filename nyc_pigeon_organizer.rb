f7445e71505e9e8bc0e26802054290154241c1f4
Jesse McReady <jessemcready@gmail.com>
2018-06-30 03:59:13 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  # holds the names of all our birds 
   names = []
+  # holds the reorganized hash 
   oranized_birds = {}
   
+  # go through the data and extract all the names of the birds
   data.each do |characteristic, value|
     value.each do |secondary_char, names_of_bird|
       names_of_bird.each do |name|
@@ -11,10 +14,14 @@ def nyc_pigeon_organizer(data)
     end
   end
   
+  # remove duplicate names 
   names.uniq!
   
+  # for each name in our names array
   names.each do |name|
+    # create a new entry in our organized hash that is a blank hash 
     oranized_birds[name] = {}
+    # give each new entry a key=>value of char(which is color/gender/lives)=>[]
     data.each do |char, value|
       oranized_birds[name][char] = []
     end 
