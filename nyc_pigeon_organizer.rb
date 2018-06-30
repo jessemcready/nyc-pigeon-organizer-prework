d5b3787cd635302e1eacc2957951b6fc71492229
Jesse McReady <jessemcready@gmail.com>
2018-06-30 03:48:16 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names = []
+  
+  data.each do |characteristic, value|
+    value.each do |secondary_char, names|
+      names.each do |name|
+        names << name
+      end 
+    end
+  end
+  
+  names.uniq!
+  
+  puts names
+  
 end
